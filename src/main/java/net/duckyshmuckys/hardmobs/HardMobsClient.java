beded671790335ca740c1331434b6e2eba8ea938
coloradoc <104806606+coloradoc@users.noreply.github.com>
2024-08-23 23:30:09 -0400

    Initial commit

---
+++ src/main/java/net/duckyshmuckys/hardmobs/HardMobsClient.java
@@ -0,0 +1,11 @@
+package net.duckyshmuckys.hardmobs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HardMobsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/duckyshmuckys/hardmobs/HardMobsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.duckyshmuckys.hardmobs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HardMobsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
